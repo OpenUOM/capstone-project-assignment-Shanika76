8153a87a5a96e1a2847dfb3c2a4b456ad25ef4b2
Shanika76 <shanikaabeyratne1976@gmail.com>
2025-01-29 21:28:25 +0530

    Update student-table.component.ts

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -51,17 +51,19 @@ export class StudentTableComponent implements OnInit {
     })
   }
 
-  search(value) {
+  search(value: string) {
     let foundItems = [];
+    
     if (value.length <= 0) {
       this.getStudentData();
     } else {
-      let b = this.studentData.filter((student) => {
-        if (student[0].name.toLowerCase().indexOf(value) > -1) {
-          foundItems.push(student)
+      foundItems = this.studentData.filter((student) => {
+        if (student[0].name.toLowerCase().includes(value.toLowerCase())) {
+          return true;
         }
+        return false;
       });
-      this.studentData = foundItems;
+      this.studentData = foundItems;  
     }
   }
 }
